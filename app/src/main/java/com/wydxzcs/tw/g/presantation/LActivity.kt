2accc7bef666e066c255ba51dcaf69bc4ad5b390
Oleg <vishnevskiyoleg@gmail.com>
2023-10-13 18:43:26 +0300

    splash screen added

--- app/src/main/java/com/wydxzcs/tw/g/presantation/LActivity.kt
+++ app/src/main/java/com/wydxzcs/tw/g/presantation/LActivity.kt
@@ -3,6 +3,7 @@ package com.wydxzcs.tw.g.presantation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.wydxzcs.tw.g.databinding.ActivityLBinding
 import kotlinx.coroutines.delay
@@ -13,6 +14,7 @@ class LActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityLBinding.inflate(layoutInflater)
         setContentView(binding.root)
